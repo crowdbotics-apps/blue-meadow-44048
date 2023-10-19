e0209722f11d4a7845a231e4170bd2b908e0aced
Crowdbotics <team@crowdbotics.com>
2023-10-19 18:37:24 +0000

    Initial commit

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "blue-meadow-44048",
+  url: "https://blue-meadow-44048.botics.co",
+  api: "https://blue-meadow-44048.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
